b8bd63b3e481c86e1bb92d15b68d20ac8d999867
carla <carlialcorta.ca@gmail.com>
2024-04-03 00:58:46 -0300

    se añade carpeta config y se corrigen errores

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { MONGO_CONFIG, EXPRESS_CONFIG } = require('./config')
+
 const app = express()
 
 mongoose
-    .connect('mongodb+srv://agenda-de-citasRollingCode:12345rollingcode@cluster0.5wch4gy.mongodb.net/agenda-de-citas?retryWrites=true&w=majority&appName=Cluster0')        
+    .connect(MONGO_CONFIG.URI)        
     .then(() => console.log('Conectado'))
     .catch(() => console.log('No Conectado'))
-app.listen(6060, () => console.log('Server listening to port', 6060))
+app.listen(EXPRESS_CONFIG.PORT, () => console.log('Server listening to port', EXPRESS_CONFIG.PORT))
